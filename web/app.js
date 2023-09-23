1c7e100daab17dbf3ee842e0277880abfaca83ab
seo_jangho@radcns.com <seo_jangho@radcns.com>
2023-09-23 18:39:59 +0900

    - 4주차 수업

--- web/app.js
+++ web/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 var booksRouter = require('./routes/books');
+var localsRouter = require('./routes/locals');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/books', booksRouter);
-
+app.use('/locals', localsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
