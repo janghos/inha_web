dcf748381cdcae65b4854775354cd0e3fa48c5ae
seo_jangho@radcns.com <seo_jangho@radcns.com>
2023-10-07 18:38:36 +0900

    5주차

---
+++ web/public/javascripts/firebase.js
@@ -0,0 +1,22 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyBC8M1YTCqC8zrfzftMBZXdQudPTCvD4-g",
+authDomain: "inhaweb-7ec64.firebaseapp.com",
+projectId: "inhaweb-7ec64",
+storageBucket: "inhaweb-7ec64.appspot.com",
+messagingSenderId: "264197381913",
+appId: "1:264197381913:web:335a4388f493a0a877ef88",
+measurementId: "G-J7L605SRTS"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- web/routes/users.js
+++ web/routes/users.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resourcasdsade');
 });
+//로그인 페이지 이동
+router.get('/login', function(req, res, next) {
+  res.render('index', {title:'로그인', pageName:'users/login.ejs'});
+});
 
+//회원가입 페이지
+router.get('/join', function(req, res, next) {
+  res.render('index', {title:'회원가입', pageName:'users/join.ejs'});
+});
 module.exports = router;
